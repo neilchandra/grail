63f22402c490d989f29ff26c85fc2b0f8f6628f4
Kevin <kevin_anderson@brown.edu>
2015-02-23 01:03:33 -0500

    4th commit testing github from Kevin's laptop

--- Grail/src/grailgames/StatusAdvance.java
+++ Grail/src/grailgames/StatusAdvance.java
@@ -2,7 +2,7 @@ package grailgames;
 
 public class StatusAdvance extends TrainingCard implements ActUponDueler {
 	
-	//bleh
+	//si se puede
 	@Override
 	public void actUpon(Dueler dueler) {
 		dueler.advanceLevel();		
