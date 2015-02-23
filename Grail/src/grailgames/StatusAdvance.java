c68b10ffaecdffb20f7051cfe0c143f26ad8e1ee
Kevin <kevin_anderson@brown.edu>
2015-02-23 01:29:03 -0500

    Commoit testing

--- Grail/src/grailgames/StatusAdvance.java
+++ Grail/src/grailgames/StatusAdvance.java
@@ -2,7 +2,7 @@ package grailgames;
 
 public class StatusAdvance extends TrainingCard implements ActUponDueler {
 
-	//this should have trouble mergin
+	//this should have trouble mergin 2
 	@Override
 	public void actUpon(Dueler dueler) {
 		dueler.advanceLevel();		
