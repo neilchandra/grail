06b8e4acb9ca6c8cb391e810240a79f84b878b27
Kevin <kevin_anderson@brown.edu>
2015-02-24 16:47:49 -0500

    Player class with play methods and commented swtich

--- Grail/src/grailgames/Player.java
+++ Grail/src/grailgames/Player.java
@@ -1,6 +1,7 @@
 package grailgames;
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
 
 public class Player {
 
@@ -13,12 +14,66 @@ public class Player {
 		this.hand = new ArrayList<Card>();
 		this.deck = deck;
 	}
-
-	public void play(int card) {
-		Card playCard = hand.get(card);
-		// playCard.play()
+	
+	/** Find the next available spot in the field
+	 * @return - the index of the available spot or -1 if field full 
+	 */
+	public int findEmptySpot(){
+		for(int x = 0; x < 7; x++) {
+			if(field[x] == null) {
+				return x;
+			}
+		}
+		return -1;
+	}
+ 
+	/** Plays Dueler card, moving it into the arena,
+	 *  if empty, or to the bench, if not
+	 *
+	 * @param dueler - the dueler to be played
+	 */
+	public void play(Dueler dueler) {
+		int index = findEmptySpot();
+		if(index != -1){
+			field[index] = dueler;
+		} else {
+			System.out.println("Your field is full! You cannot put a dueler on the field.");
+		}
+	}
+	
+	/** Switches dueler in the arena with the dueler from the bench
+	 * 
+	 *  @param index - index of Dueler on the bench */
+	public void switchDueler(int index) {
+/*		if(field[0] != null) {
+			if(field[index] != null) {
+			Dueler tempD = field[index];
+			field[index] = field[0];
+			field[0] = tempD;
+			} else {
+				
+			}
+		} else {
+			System.out.println("There is no dueler in the arena. You cannot switch!");
+		} */
 	}
 
+	/** Plays camelotReinforcement training card */
+	public void play(CamelotReinforcements camReinforcement) {
+		camReinforcement.actUpon(this);
+	}
+
+	/**
+	 * Plays cards that implements ActUponDueler (AnthraxAsylum,
+	 * HolyHealthPotion, JoustingPractice and StatusAdvance)
+	 * 
+	 * @param trainCard - The training card to be played
+	 * @param dueler - The dueler which the training card acts upon
+	 */
+	public void play(ActUponDueler trainCard, Dueler dueler) {
+		trainCard.actUpon(dueler);
+	}
+	
 	/** Draws cards from deck and places in hand */
 	public void drawCard() {
 		if (deck.isEmpty()) {
