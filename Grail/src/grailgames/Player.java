1ac5d345089bee0d8355457a15f788eafa14f712
Kevin <kevin_anderson@brown.edu>
2015-02-23 23:33:35 -0500

    Latest Player class

--- Grail/src/grailgames/Player.java
+++ Grail/src/grailgames/Player.java
@@ -1,8 +1,22 @@
 package grailgames;
 
+import java.util.ArrayList;
+
 public class Player {
 	
-	Dueler[] field = new Dueler[7];
+	Dueler[] field;
+	ArrayList<Card> hand;
+	ArrayList<Card> deck;
+	
+	public Player(ArrayList<Card> deck) {
+		this.field = new Dueler[7];
+		this.hand = new ArrayList<Card>();
+		this.deck = deck;	
+	}
 
+	public void play(int card) {
+		Card playCard = hand.get(card); 
+		// playCard.play()
+	}
 	
 }
