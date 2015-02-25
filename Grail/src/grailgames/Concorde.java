71058276e891711de4e25ec603a8113bb34a6495
neilchandra <ncsnowboarder@gmail.com>
2015-02-24 20:01:35 -0500

    added Concorde

---
+++ Grail/src/grailgames/Concorde.java
@@ -0,0 +1,60 @@
+package grailgames;
+
+public class Concorde extends BraveDueler {
+
+	//stages
+	private final String firstStage = "Peasant";
+	private final String secondStage = "CC";
+	
+	//attacks
+	private final String firstAttack = "Brave Bite";
+	private final String secondAttack = "Concorde's Conqueror";
+	
+	//damage
+	private final int firstDamage = 30;
+	private final int secondDamage = 30;
+	
+	//hp limits
+	private final int firstMaxHP = 40;
+	private final int secondMaxHP = 70;
+
+
+	public Concorde() {
+		this.name = "Concorde";
+	}
+	
+	@Override
+	public void advanceLevel() {
+		switch (currentStage) {
+			case "Page" :
+				currentStage = secondStage;
+				currentAttack = secondAttack;
+				currentDamage = secondDamage;
+				xp = 0;
+				break;
+			default :
+				//do nothing
+				break;
+		}
+	}
+	
+	@Override
+	public void attack(BraveDueler dueler) {
+		dueler.subHP(currentDamage);
+	}
+
+	@Override
+	public void attack(CowardlyDueler dueler) {
+		dueler.subHP(currentDamage - 20);
+	}
+
+	@Override
+	public void attack(PureDueler dueler) {
+		dueler.subHP(currentDamage);
+	}
+
+	@Override
+	public void attack(RoyalDueler dueler) {
+		dueler.subHP(currentDamage + 20);
+	}
+}
