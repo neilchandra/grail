b4784b3fd5800e883421f4bf4b7e516604cbbaa3
neilchandra <ncsnowboarder@gmail.com>
2015-02-26 12:30:28 -0500

    mad changes yo

--- Grail/src/grailgames/HolyHealthPotion.java
+++ Grail/src/grailgames/HolyHealthPotion.java
@@ -1,19 +1,35 @@
 package grailgames;
 
+import java.io.IOException;
+
 public abstract class HolyHealthPotion extends TrainingCard {
 	
 	public abstract void heal(Dueler dueler);
 	
 	@Override
 	public void play(Camelot c) {
-		// read in the index for dueler, P
-		
-	//	if(player.field[P] != null) {
-		//	heal(player.field[P]);
-		//} else {
-	//		System.out.println("No effet.");
-		//}
-		
-//		player.hand.remove(index);		
+		System.out.println("Select a Dueler:");
+		System.out.println("0 for the battling Duelers, 1-6 for the bench");
+		try {
+			String lineUser = user.readLine();
+			if(lineUser.equals("")){
+				System.out.println("Invalid input!  Please try again:");
+				play(c);
+			} else {
+				String [] words = lineUser.split(" ");
+				int index = Integer.parseInt(words[0]); 
+				//it works if it reaches here
+				if(index >= 0 && index < 7) {
+					//do work of training card
+				} else {
+					System.out.println("Invalid input!  Please try again:");
+					play(c);
+				}
+			}
+		} catch(Exception e) {
+			System.out.println("Invalid input!  Please try again:");
+			play(c);
+		}
+				
 	}
 }
--- Grail/src/grailgames/TrainingCard.java
+++ Grail/src/grailgames/TrainingCard.java
@@ -1,6 +1,11 @@
 package grailgames;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public abstract class TrainingCard extends Card {
+	
+	BufferedReader user = new BufferedReader(new InputStreamReader(System.in));
 	
 	@Override
 	public String toString() {
