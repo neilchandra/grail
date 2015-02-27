bb0041db86e3fe2ebbbb5ac6cb34522a531e5383
neilchandra <ncsnowboarder@gmail.com>
2015-02-27 12:11:36 -0500

    knight robin now has special powers

--- Grail/src/grailgames/KnightRobin.java
+++ Grail/src/grailgames/KnightRobin.java
@@ -18,5 +18,48 @@ public class KnightRobin extends CowardlyDueler {
 		System.out.println(name + " is at its highest level; cannot advance!");
 		return this;
 	}
+	
+	@Override
+	public void attackedByBrave(int _damage) {
+		if(arenaXP < 2) {
+			System.out.println("Knight Robin used Invisibility to block the attack!");
+		} else {
+			System.out.println("It's not very effective...");
+			if(_damage - 20 > 0){
+				subHP(_damage);
+			} else {
+				subHP(0);			
+			}			
+		}
+
+	}
+
+	@Override
+	public void attackedByCowardly(int _damage) {
+		if(arenaXP < 2) {
+			System.out.println("Knight Robin used Invisibility to block the attack!");
+		} else {
+			subHP(_damage);			
+		}
+	}
+
+	@Override
+	public void attackedByPure(int _damage) {
+		if(arenaXP < 2) {
+			System.out.println("Knight Robin used Invisibility to block the attack!");
+		} else {
+			System.out.println("It was very effective!");
+			subHP(_damage + 20);
+		}
+	}
+
+	@Override
+	public void attackedByRoyal(int _damage) {
+		if(arenaXP < 2) {
+			System.out.println("Knight Robin used Invisibility to block the attack!");
+		} else {
+			subHP(_damage);			
+		}
+	}
 
 }
