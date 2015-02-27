a8b46a8d253f5aa3f442de87db82abdc82a5f631
neilchandra <ncsnowboarder@gmail.com>
2015-02-26 23:53:14 -0500

    camelot refinforcements

--- Grail/src/grailgames/CamelotReinforcements.java
+++ Grail/src/grailgames/CamelotReinforcements.java
@@ -4,6 +4,7 @@ public class CamelotReinforcements extends TrainingCard {
 
 	@Override
 	public void play(Game c) {
+		System.out.println(c.currentPlayer.getName()+" used Camelot Reinforcements!");
 		c.currentPlayer.drawCard();
 		c.currentPlayer.drawCard();
 		c.currentPlayer.drawCard();
